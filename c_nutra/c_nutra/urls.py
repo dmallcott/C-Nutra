f5e938cca87b9dfb47fd3b59b8c0b634957f142c
dmallcott <daniel.mallcott@gmail.com>
2014-05-10 20:25:51 -0430

    Changes the base html. Began separating it into usable pieces and made it work with Django's template language.

--- c_nutra/c_nutra/urls.py
+++ c_nutra/c_nutra/urls.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
 
     # Examples:
     # url(r'^$', 'c_nutra.views.home', name='home'),
@@ -18,7 +18,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls))
+
 )
 
 # Uncomment the next line to serve media files in dev.
