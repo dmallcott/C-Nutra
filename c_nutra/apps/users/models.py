bcf349883e7a781ac00d2e59dfb8c0e6e1f899ec
dmallcott <daniel.mallcott@gmail.com>
2014-06-08 17:01:12 -0430

    Changes from Ismael's mail.
    - Following PEP8
    - Removed our personal logout and used contrib auth
    - Moved some forms from models to forms
    - Little details to simplify code reading

--- c_nutra/apps/users/models.py
+++ c_nutra/apps/users/models.py
@@ -1,41 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from datetime import date
+
 
 class UserProfile(models.Model):
-	user = models.OneToOneField(User)  
-	#other fields here
-	birthday = models.DateField(default=date.today)
-	gender = models.CharField(max_length=1, choices=(('M', 'Masculino'), ('F', 'Femenino')))
-	height = models.IntegerField()
-	weight = models.IntegerField()
-	elbow_diameter = models.IntegerField()
+    user = models.OneToOneField(User)
+    # other fields here
+    birthday = models.DateField(auto_now=True)
+    gender = models.CharField(
+        max_length=1,
+        choices=(
+            ('M', 'Masculino'),
+            ('F', 'Femenino')
+        )
+    )
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    elbow_diameter = models.IntegerField()
 
-	def __str__(self):  
-		return "%s's profile" % self.user  
+    def __unicode__(self):
+        return u'Perfil de %s' % self.user
 
-	def create_user_profile(sender, instance, created, **kwargs):  
-		if created:  
-			profile, created = UserProfile.objects.get_or_create(user=instance)  
-
-class UserForm(forms.Form):
-	username = forms.CharField(required=False)
-	email = forms.EmailField(required=False,
-		error_messages={'invalid': u'Formato incorrecto de email.'})
-	first_name = forms.CharField(required=False)
-	last_name = forms.CharField(required=False)
-
-
-class UserProfileForm(forms.Form):
-	birthday = forms.DateField(required=False, input_formats=["%d-%m-%Y"], 
-		error_messages={'invalid': u'El formato de la fecha es dd-mm-yyyy.'})
-	gender = forms.CharField(required=False, widget=forms.Select(choices=(('M', 'Masculino'), ('F', 'Femenino'))))
-	height = forms.IntegerField(required=False, min_value=1,
-		error_messages={'invalid': u'Esto no pasa con html5.', 'min_value': u'La altura no puede ser negativa.'})
-	weight = forms.IntegerField(required=False, min_value=1,
-		error_messages={'invalid': u'Esto no pasa con html5.', 'min_value': u'El peso no puede ser negativo.'})
-	elbow_diameter = forms.IntegerField(required=False, min_value=1,
-		error_messages={'invalid': u'Esto no pasa con html5.', 'min_value': u'El diametro del codo no puede ser negativo.'})
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            profile, created = UserProfile.objects.get_or_create(user=instance)
--- c_nutra/apps/users/views.py
+++ c_nutra/apps/users/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
-#Import a user registration form
+# Import a user registration form
 from apps.users.forms import UserRegisterForm
-from apps.users.models import UserProfile, UserProfileForm, UserForm
+from apps.users.models import UserProfile,
+from apps.users.forms import UserProfileForm, UserForm
 from django.contrib.auth.models import User
 
 from django.contrib.auth.middleware import get_user
@@ -15,12 +16,6 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.messages import get_messages
 
-
-
-# User Logout View
-def user_logout(request):
-    logout(request)
-    return HttpResponseRedirect('/')
 
 # User Register View
 def user_register(request):
@@ -30,17 +25,17 @@ def user_register(request):
             if form.is_valid:
                 form.save()
                 return HttpResponse('User created succcessfully.')
+                # no good. fix this
         else:
             form = UserRegisterForm()
         context = {}
         context.update(csrf(request))
         context['form'] = form
-        #Pass the context to a template
+        # Pass the context to a template
         return render_to_response('registration/register.html', context)
     else:
         return HttpResponseRedirect('/')
 
-# 
 @login_required
 def user_profile(request):
     if request.method == 'POST':
@@ -57,10 +52,14 @@ def user_profile(request):
                 if email:
                     User.objects.filter(id=user.id).update(email=email)
                 if first_name:
-                    User.objects.filter(id=user.id).update(first_name=first_name)
+                    User.objects.filter(id=user.id).update(
+                        first_name=first_name)
                 if last_name:
                     User.objects.filter(id=user.id).update(last_name=last_name)
-                messages.add_message(request, messages.SUCCESS, 'Perfil actualizado correctamente.', extra_tags={'user':'user'})
+                messages.add_message(
+                    request, messages.SUCCESS, 
+                    'Perfil actualizado correctamente.', 
+                    extra_tags={'user': 'user'})
             else:
                 for field in form:
                     for error in field.errors:
@@ -74,7 +73,8 @@ def user_profile(request):
                 weight = form.cleaned_data.get('weight')
                 elbow_diameter = form.cleaned_data.get('elbow_diameter')
                 if birthday:
-                    UserProfile.objects.filter(user=user).update(birthday=birthday)
+                    UserProfile.objects.filter(
+                        user=user).update(birthday=birthday)
                 if gender:
                     UserProfile.objects.filter(user=user).update(gender=gender)
                 if height:
@@ -82,12 +82,21 @@ def user_profile(request):
                 if weight:
                     UserProfile.objects.filter(user=user).update(weight=weight)
                 if elbow_diameter:
-                    UserProfile.objects.filter(user=user).update(elbow_diameter=elbow_diameter)
-                messages.add_message(request, messages.SUCCESS, 'Perfil actualizado correctamente.', extra_tags=('profile'))
+                    UserProfile.objects.filter(user=user).update(
+                        elbow_diameter=elbow_diameter)
+
+                messages.add_message(
+                    request, messages.SUCCESS, 
+                    'Perfil actualizado correctamente.', 
+                    extra_tags=('profile'))
             else:
                 for field in form:
                     for error in field.errors:
-                        messages.add_message(request, messages.ERROR, error, extra_tags=('profile'))
+                        messages.add_message(
+                            request, messages.ERROR, 
+                            error, 
+                            extra_tags=('profile'))
+
         return HttpResponseRedirect('/accounts/profile/')
 
     else:
@@ -99,5 +108,13 @@ def user_profile(request):
         for message in messages_temp:
             if 'profile' in message.tags:
                 profile_messages = True
-        return render(request, 'profile/profile.html',
-            {'profile':profile, 'form_profile':form_profile, 'form_user': form_user, 'profile_messages':profile_messages})
+        return render(
+            request, 
+            'profile/profile.html',
+            {
+                'profile': profile, 
+                'form_profile': form_profile, 
+                'form_user': form_user, 
+                'profile_messages': profile_messages
+            }
+        )
