c4430de0def98e5b940132ea3407f667ebf2960b
Gustavo Siñovsky <g.sinovsky@gmail.com>
2014-06-08 23:06:59 -0430

    Breve avance en interfaz, implementada una lista de dropdowns basicos para cada poll, falta mejorar colores y logicamente enlazar con las polls de verdad

--- c_nutra/config/urls.py
+++ c_nutra/config/urls.py
@@ -29,6 +29,7 @@ urlpatterns = patterns('',
     url(r'accounts/logout/$', 'apps.users.views.user_logout', name='logout'),
     url(r'accounts/register/$', 'apps.users.views.user_register', name='register'),
     url(r'accounts/profile/$', 'apps.users.views.user_profile', name='profile'),
+    url(r'encuestas/$',TemplateView.as_view(template_name='surveys/surveys.html'), name='surveys')
 
     # Examples:
     # url(r'^$', 'c_nutra.views.home', name='home'),
